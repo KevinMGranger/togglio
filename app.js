60e386e09d046c1bbd18aee97263a6bd6118ad29
Craig Cabrey <craigcabrey@gmail.com>
2015-04-18 16:54:34 -0400

    Clean up generator cruft

--- app.js
+++ app.js
@@ -10,15 +10,8 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -55,6 +48,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.json({ success: true });
+});
+
 router.get('/:user', function(req, res, next) {
   res.json(req.params.user);
 });
@@ -20,6 +25,19 @@ router.post('/:user/:resource/on', function(req, res, next) {
 
 router.post('/:user/:resource/off', function(req, res, next) {
   console.log('off');
+});
+
+router.get('/:user/:resource/hooks', function(req, res, next) {
+  req.db.query(
+    '',
+    function(err, rows, fields) {
+      res.json(rows);
+    }
+  );
+});
+
+router.post('/:user/:resource/hooks', function(req, res, next) {
+
 });
 
 module.exports = router;
