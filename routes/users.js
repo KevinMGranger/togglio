df74270a49b6b8a6de513d5a4b150041e38d4a04
Craig Cabrey <craigcabrey@gmail.com>
2015-04-18 21:37:49 -0400

    Initial implementation of users route

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 router.post('/', function(req, res, next) {
   console.log(req.body);
   res.json({ success: true });
 });
 
-router.get('/:user', function(req, res, next) {
-  res.json(req.params.user);
+router.get('/:user/resources', function(req, res, next) {
+  req.db.query(
+    'SELECT resourceName, statusID FROM resources WHERE userId = ?',
+    [req.params.user],
+    function(err, rows, fields) {
+      res.json(rows);
+    }
+  );
 });
 
-router.post('/:user/:resource/toggle?', function(req, res, next) {
+router.post('/:user/resources', function(req, res, next) {
+  var record = {
+    userID: req.params.user,
+    resourceName: req.body.resource,
+    statusID: 0
+  };
+
+  req.db.query(
+    'INSERT INTO resources SET ?',
+    record,
+    function(err, rows, fields) {
+
+    }
+  );
+});
+
+router.post('/:user/:resource/toggle', function(req, res, next) {
   console.log('post resource/toggle');
 });
 
